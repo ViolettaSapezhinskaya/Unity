4a00dde78e9c8e6ffbc3c7c4b36d869984ee3e91
Arinasap10@gmail.com <Arinasap10@gmail.com>
2022-02-05 21:22:00 +0300

    hti

---
+++ Assets/Scenes/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var a = Input.GetAxis("Horizontal");
+        var b = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(a, 0, b);
+        //Задание позиции 1 раз
+        //transform.position = transform.position + Speed *direction*Time.deltaTime;
+
+        transform.Translate(Speed * direction * Time.deltaTime);
+    }
+}
